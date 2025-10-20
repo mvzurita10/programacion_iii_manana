0d3ec57c4f211a5276f97d00f5618a7e47b5f841
mvzurita10 <mikyzurita65@gmail.com>
2025-10-20 18:14:43 -0500

    Tarea Semana3

---
+++ modulo2/javascript/02_variables_mp.js
@@ -0,0 +1,46 @@
+console.log("Control de Puertos Marítimos MV");
+
+nombreBarco = "Santa María"
+console.log("Nombre del Barco:", nombreBarco);
+console.log("Tipo:", typeof(nombreBarco));
+
+
+numeroContenedores = 350
+console.log("Número de Contenedores:", numeroContenedores);
+console.log("Tipo:", typeof(numeroContenedores));
+
+
+permisoAtracar = true
+console.log("Permiso para Atracar:", permisoAtracar);
+console.log("Tipo:", typeof(permisoAtracar));
+
+
+horaLlegada = undefined
+console.log("Hora de Llegada:", horaLlegada);
+console.log("Tipo:", typeof(horaLlegada));
+
+
+muelleAsignado = null
+console.log("Muelle Asignado:", muelleAsignado);
+console.log("Tipo:", typeof(muelleAsignado));
+
+
+cargaTotal = 9876543210n
+console.log("Carga Total (kg):", cargaTotal);
+console.log("Tipo:", typeof(cargaTotal));
+
+
+barco = {
+    nombre: "Santa María",
+    tipo: "Carga Pesada",
+    bandera: "Ecuador",
+    capacidadToneladas: 50000
+}
+console.log("Datos del Barco:", barco);
+console.log("Tipo:", typeof(barco));
+
+
+muellesDisponibles = ["Muelle Norte", "Muelle Sur", "Muelle Este", "Muelle Oeste"]
+console.log("Muelles Disponibles:", muellesDisponibles);
+console.log("Tipo:", typeof(muellesDisponibles)); 
+
