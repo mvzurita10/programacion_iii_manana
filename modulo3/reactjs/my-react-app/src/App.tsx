ade11b98f00309fc216619434e5b8d6f58635e2b
mvzurita10 <mikyzurita65@gmail.com>
2025-12-15 10:07:23 -0500

    Clase 15/12/2025

--- modulo3/reactjs/my-react-app/src/App.tsx
+++ modulo3/reactjs/my-react-app/src/App.tsx
@@ -1,13 +1,22 @@
 
-import MultiSwitch from './useState/MultiSwitch';
+import { useState } from 'react';
+import { LanguageContext } from './useContext/LanguageContext';
+import LanguageToggle from './useContext/LanguageToggle';
+import { LoginContext } from './useContext/LoginContext';
+import LoginStatus from './useContext/LoginStatus';
 
+export default function App() {
+  const [lang, setLang] = useState('es');
+  const toggleLanguage = () => setLang(prev => (prev === 'es' ? 'en' : 'es'));
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const toggleLogin = () => setIsLoggedIn(prev => !prev);
 
-function App() {
   return (
     <>
-      <MultiSwitch />
-    </>
+      <LanguageContext.Provider value={{ lang, toggleLanguage }}>
+          <LanguageToggle />
+      </LanguageContext.Provider><LoginContext.Provider value={{ isLoggedIn, toggleLogin }}>
+          <LoginStatus />
+      </LoginContext.Provider></>
   );
 }
-
-export default App;
