ee88005b3ea6466fd424755987a6d8225e61fde7
mvzurita10 <mikyzurita65@gmail.com>
2026-01-12 09:58:56 -0500

    Clase 12/01/2026

---
+++ modulo3/reactjs/react-styles-demo/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/global.css";
+import "./theme/theme.css";
+
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
